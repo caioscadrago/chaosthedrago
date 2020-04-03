701830cda9d407444a6bcae98719551b0ed8bccb
caioscadrago <caios.siqueira@gmail.com>
2020-04-02 21:46:40 -0300

    breakout

---
+++ src/Powerup.lua
@@ -0,0 +1,35 @@
+Powerup = Class{}
+
+function Powerup:init(skin)
+	self.width = 16
+	self.height=16
+
+	self.dy = 20
+	self.dx=0
+
+	self.skin = skin
+	self.inPlay = true
+end
+
+function  Powerup:collides(target)
+	
+	if self.x > target.x + target.width or target.x > self.x + self.width then
+		return false
+	end
+
+	if self.y> target.y + target.height or target.y > self.y +self.height then
+		return false
+	end
+
+	return true
+end
+
+function  Powerup:update(dt)
+	self.x = self.x + self.dx * dt
+	self.y = self.y + self.dy * dt
+end
+
+function Pwerup:render()
+	love.graphics.draw(qTextures['main'], gFrames['powerups'][self.skin],
+		self.x,self.y)
+end
